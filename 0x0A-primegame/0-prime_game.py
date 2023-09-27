35b873161e3ae362e070598de0d0992fc6c5d2de
manuel4life <allotey4eva@gmail.com>
2023-09-27 17:37:52 +0000

    files

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+def primeNumbers(n):
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
+
+
+def isWinner(x, nums):
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = Ben = 0
+    for i in range(x):
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
